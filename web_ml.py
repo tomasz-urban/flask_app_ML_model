14a19a65a83f6baa6890cbdc9f4a5a44faca1ce3
unknown <tomasz.urban.py@gmail.com>
2021-12-09 09:39:01 +0100

    Editorial changes

--- web_ml.py
+++ web_ml.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory, request
+from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
@@ -6,11 +6,13 @@ app = Flask(__name__)
 
 model = pickle.load(open('heart_disease_random_forest.joblib', 'rb'))
 
+
 @app.route("/")
 def home_page():
     return render_template('index.html')
 
-@app.route('/predict_heart_disease', methods = ['POST'])
+
+@app.route('/predict_heart_disease', methods=['POST'])
 def prediction_data():
     age = request.form['age']
     sex = request.form['sex']
@@ -25,26 +27,16 @@ def prediction_data():
     slope = request.form['slope']
     ca = request.form['ca']
     thal = request.form['thal']
+
     input_array = np.array([[age, sex, cp, trestbps, chol, fbs, restcg, thalach, exang, oldpeak, slope, ca, thal]])
     pred = model.predict(input_array)
     if pred == 1:
-        return
-    "According to the provided data You have a heart disease. Please contact a doctor immediately. " \
-    "[NOTE: Remember that this app is only for educational purposes. " \
-    "This result doesn't provide any medical indication or contraindication for treatment!"
+        return render_template('positive.html')
+    # "According to the provided data You have a heart disease. Please contact a doctor immediately. " \
+    # "[NOTE: Remember that this app is only for educational purposes. " \
+    # "This result doesn't provide any medical indication or contraindication for treatment!"
     else:
-        return
-    "According to the provided data You don't have a heart disease." \
-    "[NOTE: Remember that this app is only for educational purposes. " \
-    "This result doesn't provide any medical indication or contraindication for treatment!"
-
-
-
-
-
-# @app.route("/static/<path:path>")
-# def static_dir(path):
-#     return send_from_directory("static", path)
-#
-# @app.route("/ML/heart_disease", methods=['POST'])
-# def heart_disease():
+        return render_template('negative.html')
+    # "According to the provided data You don't have a heart disease." \
+    # "[NOTE: Remember that this app is only for educational purposes. " \
+    # "This result doesn't provide any medical indication or contraindication for treatment!"
